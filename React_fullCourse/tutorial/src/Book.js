cdec117aef5307a5287a6cd4adf9ead5d01ef4ec
Digyash kalita <www.digyash.kalita1@gmail.com>
2021-05-23 20:32:41 +0530

    modules created

---
+++ React_fullCourse/tutorial/src/Book.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+
+//Create Book component
+const Book = ({img, title, author}) =>{
+// const Book = (props) =>{
+// const Book = (/*{img, title, author}*/) =>{
+
+// const {img, title, author} = props.book;
+// attribute, onMousehandler
+// onClick, onMouseOver
+
+  return (
+    <article className="book"   onMouseOver ={ ()=>{
+      console.log(title)
+    }}>
+       <img src={img} alt="" />
+       <h1>{title}</h1>
+      {/* {props.children} */} 
+      <h4>{author}</h4>
+      {/* <p>{props.job}</p>
+      <p>{props.title}</p> */}
+    </article>
+  )
+}
+
+
+// const Image = () => {
+//   return(
+//   <img src="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg" alt=""/>
+//   )
+// }
+
+// const Title = () =>{
+//   return(
+//     <h1>Atomic Habits: The life-changing million copy bestseller</h1>
+//   )
+// }
+
+// const  Author = () =>{
+//   return(
+//     <h4 style={{color: '#617d98', fontSize:"0.75rem", marginTop:"0.25rem"}}>James Clear</h4>
+//   )
+// }
+export default Book
--- React_fullCourse/tutorial/src/index.js
+++ React_fullCourse/tutorial/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import {books} from './books';
+import Book from './Book'
 //css
 import "./index.css" ;
 
+
+//Create booklists
+function BookList(){
+  return(
+    <section className="booklist">
+      {books.map((book) =>{
+        return <Book key={book.id} /*book ={book}*/ {...book}></Book>
+      })}
+    </section>
+  )
+}
 // setup vars
 // const author ="James Clear";
 // const title="Atomic Habits: The life-changing million copy bestseller";
@@ -19,27 +32,6 @@ import "./index.css" ;
 // author: "Héctor García, Francesc Miralles "
 // }
 
-//books array
-const books=[
-          {
-        id:1,
-        img:"https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg" ,
-        title: "Atomic Habits: The life-changing million copy bestseller",
-        author: "James Clear"
-        },
-        {
-        id:2,
-        img:"https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL200_SR200,200_.jpg" ,
-        title: "Ikigai: The Japanese secret to a long and happy life",
-        author: "Héctor García, Francesc Miralles "
-        },
-        {
-        id:3,
-        img:"https://images-eu.ssl-images-amazon.com/images/I/713jIoMO3UL._AC_UL200_SR200,200_.jpg" ,
-        title: "Sapiens: A Brief History of Humankind",
-        author: "Yuval Noah Harari"
-        }, 
-]
 
 
 //Stateless functional component
@@ -73,56 +65,6 @@ const books=[
 //             );
 // }
 
-//Create booklists
-function BookList(){
-  return(
-    <section className="booklist">
-      {books.map((book) =>{
-        return <Book key={book.id} /*book ={book}*/ {...book}></Book>
-      })}
-    </section>
-  )
-}
-//Create Book component
-const Book = ({img, title, author}) =>{
-// const Book = (props) =>{
-// const Book = (/*{img, title, author}*/) =>{
 
-// const {img, title, author} = props.book;
-// attribute, onMousehandler
-// onClick, onMouseOver
-
-  return (
-    <article className="book"   onMouseOver ={ ()=>{
-      console.log(title)
-    }}>
-       <img src={img} alt="" />
-       <h1>{title}</h1>
-      {/* {props.children} */} 
-      <h4>{author}</h4>
-      {/* <p>{props.job}</p>
-      <p>{props.title}</p> */}
-    </article>
-  )
-}
-
-
-// const Image = () => {
-//   return(
-//   <img src="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg" alt=""/>
-//   )
-// }
-
-// const Title = () =>{
-//   return(
-//     <h1>Atomic Habits: The life-changing million copy bestseller</h1>
-//   )
-// }
-
-// const  Author = () =>{
-//   return(
-//     <h4 style={{color: '#617d98', fontSize:"0.75rem", marginTop:"0.25rem"}}>James Clear</h4>
-//   )
-// }
 
 ReactDom.render(<BookList/>, document.getElementById("root"));  
