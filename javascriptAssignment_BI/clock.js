ac5d4faee37ec3d0dbbba05a128dde22140af560
Digyash kalita <www.digyash.kalita1@gmail.com>
2021-06-27 14:08:49 +0530

    ui modified

--- javascriptAssignment_BI/clock.js
+++ javascriptAssignment_BI/clock.js
@@ -2,10 +2,12 @@ let startElement = document.getElementById('start');
 const timerElement = document.getElementById("timer");
 let headingElement = document.getElementById("heading");
 let stopElement = document.getElementById("stop")
+let dateElement = document.getElementById("date")
 
 let timer = null;  // The timer variable is null until the clock initializes
 
 startElement.addEventListener("click", ()=>{ 
+  if(timer ==null)
     timer = setInterval(runClock, 1000);  
 });
 
@@ -22,6 +24,10 @@ stopElement.addEventListener("click", ()=>{
 
 
 function runClock() {
-    timerElement.textContent = new Date().toLocaleTimeString();            
+    timerElement.textContent = new Date().toLocaleTimeString();    
+  
+        
 }
 
+  let today = new Date().toLocaleDateString();
+    dateElement.textContent = today;
